5a3a0fd8a3314e3d91bf8ba7cf26acb8b6d501af
Genesses fernandes <102777715+Genesses12@users.noreply.github.com>
2023-09-26 13:07:21 -0400

    adicionando projeto

---
+++ src/IPhone.java
@@ -0,0 +1,77 @@
+
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    // Implementação dos métodos da interface ReprodutorMusical
+
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+    }
+
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    public void selecionarMusica() {
+        System.out.println("Música selecionada.");
+    }
+
+    // Implementação dos métodos da interface AparelhoTelefonico
+
+    public void ligar() {
+        System.out.println("Ligando o telefone...");
+    }
+
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    // Implementação dos métodos da interface NavegadorInternet
+
+    public void exibirPagina() {
+        System.out.println("Exibindo página da web...");
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba ao navegador...");
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Atualizando página da web...");
+    }
+
+    // Outros métodos específicos do iPhone
+
+    public void iniciarReprodutorMusical() {
+        System.out.println("Iniciando reprodutor musical...");
+    }
+
+    public void iniciarAparelhoTelefonico() {
+        System.out.println("Iniciando telefone...");
+    }
+
+    public void iniciarNavegadorInternet() {
+        System.out.println("Iniciando navegador de internet...");
+    }
+
+    public static void main(String[] args) {
+        IPhone iphone = new IPhone();
+
+        // Exemplos de utilização
+        iphone.iniciarReprodutorMusical();
+        iphone.tocar();
+        iphone.pausar();
+
+        iphone.iniciarAparelhoTelefonico();
+        iphone.ligar();
+        iphone.atender();
+
+        iphone.iniciarNavegadorInternet();
+        iphone.exibirPagina();
+        iphone.adicionarNovaAba();
+        iphone.atualizarPagina();
+    }
+}
